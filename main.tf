f80f981a5a447a0e5b0803eace81806f7986f00e
Ondrej Sika <ondrej@ondrejsika.com>
2021-04-20 10:23:09 +0200

    feat: Add public ip for load balancer

--- main.tf
+++ main.tf
@@ -12,7 +12,20 @@ module "ondrejsika" {
   kubernetes_version     = "1.19.7"
 }
 
+resource "azurerm_public_ip" "ingress" {
+  name                = "ondrejsika-ingress"
+  resource_group_name = module.ondrejsika.azurerm_kubernetes_cluster.node_resource_group
+  location            = module.ondrejsika.azurerm_kubernetes_cluster.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 output "kubeconfig" {
   value     = module.ondrejsika.kubeconfig
+  sensitive = true
+}
+
+output "ingress-ip" {
+  value     = azurerm_public_ip.ingress.ip_address
   sensitive = true
 }
